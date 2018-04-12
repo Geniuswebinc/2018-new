4a761be45fd741e422b8462eea21aaece1fa5b9b
ueno <ueno@genius-web.co.jp>
2018-04-12 12:17:29 +0900

    インデント

--- ueno/contact/get/confirm.php
+++ ueno/contact/get/confirm.php
@@ -1,83 +1,83 @@
 <?php
-$name1=$_GET['nameData'];
-$name2=$_GET['mailData'];
-$name3=$_GET['textData'];
+    $name1=$_GET['nameData'];
+    $name2=$_GET['mailData'];
+    $name3=$_GET['textData'];
 ?>
 
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>2018/04/09の課題</title>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>2018/04/09の課題</title>
 
   <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="./assets/css/app.css">
-  <script type="text/javascript" src="./assets/js/app.js"></script>
-</head>
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <h1>
-  お問合わせ確認
-</h1>
-    </div>
-  </div>
-</div>
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <script type="text/javascript" src="./assets/js/app.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h1>
+                        お問合わせ確認
+                    </h1>
+                </div>
+            </div>
+        </div>
 
 <!-- 名前 -->
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-    <p>
-      名前
-    </p>
-    <?php echo $name1; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>
+                    名前
+                </p>
+                <?php echo $name1; ?>
+            </div>
         </div>
-      </div>
-  </div>
+    </div>
 
 <!-- メールアドレス確認 -->
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-    <p>
-      メールアドレス
-    </p>
-      <?php echo $name2; ?>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>
+                    メールアドレス
+                </p>
+                <?php echo $name2; ?>
+            </div>
+        </div>
     </div>
-  </div>
 
 <!-- 内容入力確認 -->
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-    <p>
-      内容
-    </p>
-    <?php echo $name3; ?>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>
+                    内容
+                </p>
+                <?php echo $name3; ?>
+            </div>
+        </div>
     </div>
-  </div>
 
 <!-- 戻るボタン　送信ボタン -->
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
 
-<INPUT type="button" value="戻る" onclick="history.back()">
+                <input type="button" value="戻る" onclick="history.back()">
 
-<INPUT type="button" value="送信" onClick="location.href='thanks.php'">
+                <input type="button" value="送信" onClick="location.href='thanks.php'">
 
-      </div>
+            </div>
+        </div>
     </div>
-  </div>
 
-</body>
-</html>
+    </body>
+    </html>
--- ueno/contact/get/index.php
+++ ueno/contact/get/index.php
@@ -1,89 +1,85 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <title>2018/04/09の課題</title>
+    <head>
+    <meta charset="utf-8">
+    <title>2018/04/09の課題</title>
 
     <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
   <!-- Optional theme -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
   <link rel="stylesheet" href="./assets/css/app.css">
   <script type="text/javascript" src="./assets/js/app.js"></script>
-</head>
-<body>
+    </head>
+    <body>
 
-  <form method="get" action="confirm.php">
+    <form method="get" action="confirm.php">
 
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <h1>
-          お問合せフォーム
-        </h1>
-    </div>
-  </div>
-</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h1>
+                        お問合せフォーム
+                    </h1>
+                </div>
+            </div>
+        </div>
 
 <!-- テキストボックス -->
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-    <p>
-      名前
-    </p>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>
+                    名前
+                </p>
 
-    <input type="text" name="nameData" class="form-control" placeholder="">
+                <input type="text" name="nameData" class="form-control" placeholder="">
 
-      </div>
+            </div>
+        </div>
     </div>
-  </div>
 
 <!-- メールアドレス入力欄 -->
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-    <p>
-      メールアドレス
-    </p>
-    <input type="text" name="mailData" class="form-control" placeholder="">
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>
+                    メールアドレス
+                </p>
+                <input type="text" name="mailData" class="form-control" placeholder="">
+            </div>
+        </div>
     </div>
-  </div>
 
 <!-- 内容入力欄 -->
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-    <p>
-      内容
-    </p>
-		<textarea name="textData"  class="form-control"  id="InputTextarea" placeholder=""></textarea>
-      </div>
-    </div>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>
+                    内容
+                </p>
+		              <textarea name="textData"  class="form-control"  id="InputTextarea" placeholder=""></textarea>
+                  </div>
+              </div>
+          </div>
 
 <!-- 送信ボタン -->
-
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-
-<INPUT type="submit" value="送信" class="btn btn-success">
-
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <input type="submit" value="送信" class="btn btn-success">
+            </div>
         </div>
-      </div>
-    </div>
-
-</form>
+        </div>
+    </form>
 
  <!-- <div class="header">header</div>
  <div class="main-visual">main-visual</div>
  <div class="sidebar">sidebar</div>
  <div class="content">content</div>
  <div class="footer">footer</div> -->
-</body>
-</html>
+    </body>
+    </html>

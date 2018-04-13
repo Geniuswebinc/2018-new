e8f6597ec9a975b257a6978512c0679fea200095
fukuzono <fukuzono@genius-web.co.jp>
2018-04-13 09:44:41 +0900

    #35 課題１-問題２、問題３の実装

--- fukuzono/kadai/001.php
+++ fukuzono/kadai/001.php
@@ -5,13 +5,28 @@
 </head>
 
 <body>
-    <div method="get" id="001">
+    <div method="get" id="">
+        <?php
+            echo $_GET['id'];
+        ?>
+
+        <?php
+            if ($_GET['id'] >= 10) {
+            echo '10以上の値です';
+            }else {
+            echo '9以下の値です';
+            }
+        ?>
+
+        <?php
+            if ($_GET['id'] >= 10) {
+            echo '10以上の値です';
+            }else if ($_GET['id'] >= 1 && $_GET['id'] < 10) {
+            echo '9以下の値です';
+            }else {
+            echo '無効の値です';
+            }
+        ?>
     </div>
-
-    <?php
-        echo $_GET['id'];
-    ?>
-
-
 </body>
 </html>

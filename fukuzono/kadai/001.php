28a7bf272fbf6966a98437d437e3bb258c152340
fukuzono <fukuzono@genius-web.co.jp>
2018-04-13 13:50:39 +0900

    #35 課題1の修正_1 ①$_GETを変数に入れ、逐一$_GETと記述していた場面を修正 ②div内のmethodを削除 ③PHP部分にインデントを挿入 ④問題番号の記述

--- fukuzono/kadai/001.php
+++ fukuzono/kadai/001.php
@@ -1,3 +1,7 @@
+<?php
+    $id=$_GET['id'];
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,26 +9,29 @@
 </head>
 
 <body>
-    <div method="get" id="">
+    <div id="">
+        <p>【問題１】</p>
         <?php
-            echo $_GET['id'];
+            echo $id;
         ?>
 
+        <p>【問題２】</p>
         <?php
-            if ($_GET['id'] >= 10) {
-            echo '10以上の値です';
+            if ($id >= 10) {
+                echo '10以上の値です';
             }else {
-            echo '9以下の値です';
+                echo '9以下の値です';
             }
         ?>
 
+        <p>【問題３】</p>
         <?php
-            if ($_GET['id'] >= 10) {
-            echo '10以上の値です';
-            }else if ($_GET['id'] >= 1 && $_GET['id'] < 10) {
-            echo '9以下の値です';
+            if ($id >= 10) {
+                echo '10以上の値です';
+            }else if ($id >= 1 && $id < 10) {
+                echo '9以下の値です';
             }else {
-            echo '無効の値です';
+                echo '無効の値です';
             }
         ?>
     </div>

c97c36951fe19bf9e36ed9122f163161f6705d8b
ueno <ueno@genius-web.co.jp>
2018-04-13 13:24:33 +0900

    getのファイルデータに残っていたPOST用の記述を修正

--- ueno/contact/get/thanks.php
+++ ueno/contact/get/thanks.php
@@ -5,31 +5,31 @@ $name4=$_GET['nameDate2'];
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <title>2018/04/09の課題</title>
+    <meta charset="utf-8">
+    <title>2018/04/09の課題</title>
 
   <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
   <link rel="stylesheet" href="./assets/css/app.css">
-  <script type="text/javascript" src="./assets/js/app.js"></script>
+    <script type="text/javascript" src="./assets/js/app.js"></script>
 </head>
 <body>
 
 
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <h1>
-  お問合せ　完了
-</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>
+                    お問合せ　完了
+                </h1>
 
-<p>
-  　様お問い合せありがとうございました。
-</p>
+                <p>
+  　                 お問い合せありがとうございました。
+    </p>
     </div>
   </div>
 </div>

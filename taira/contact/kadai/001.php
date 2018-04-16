a5ee11539c94ae1cba97ce0307c29169987a5b32
taira <taira@genius-web.co.jp>
2018-04-16 14:06:26 +0900

    #20[kadai-001] 課題1の実装 改行の文字の修正、10以下を10未満に修正

--- taira/contact/kadai/001.php
+++ taira/contact/kadai/001.php
@@ -1,22 +1,39 @@
 <?php
 $Question = $_GET['id'];
+?>
+<!DOCTYPE html>
+<html>
 
-echo '問題1'.nl2br("\n").$Question.nl2br("\n");
+<head>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <script type='text/javascript' src="./assets/js/app.js"></script>
+</head>
+<body>
+<?php
+echo '問題1'.'<br>'.$Question.'<br>';
 
-echo '問題2'.nl2br("\n");
+echo '問題2'.'<br>';
 if($Question>=10){
-    echo '10以上の値です'.nl2br("\n");
-}else if($Question<=9){
-    echo '9以下の値です'.nl2br("\n");
+    echo '10以上の値です'.'<br>';
+}else if($Question<10){
+    echo '9以下の値です'.'<br>';
 };
 
-echo '問題3'.nl2br("\n");
-if(ctype_digit($Question)==false || $Question==null){
+echo '問題3'.'<br>';
+if(is_numeric($Question)==false || $Question==null){
     echo "無効の値です";
-    break;
 }else if($Question>=10){
-    echo '10以上の値です'.nl2br("\n");
-}else if($Question<=9){
-    echo '9以下の値です'.nl2br("\n");
+    echo '10以上の値です'.'<br>';
+}else if($Question<10){
+    echo '9以下の値です'.'<br>';
 };
 ?>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>

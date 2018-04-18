fc4ce5300fb8e09d5dae72595468e01cfdcf47ec
takahashi <takahashi@genius-web.co.jp>
2018-04-18 11:56:16 +0900

    #58の修正/#59

--- takahashi/contact/post/admin.php
+++ takahashi/contact/post/admin.php
@@ -1,32 +1,34 @@
 <?php
     require_once dirname(__FILE__) .'/data/require.php';
-
-    $name=$_POST['sentname'];
-    $mail=$_POST['sentmail'];
-    $content=$_POST['sentcontent'];
-    $seach=$_POST['seach'];
-
-    // データベース接続のクラス
     $conn = new DbConn();
 
-    if(isset($_POST['seach'])){
-        $sql  = 'SELECT * FROM contacts';
-        $sql .= '   WHERE name LIKE "%'.$seach.'%"';
-        $sql .= '   ORDER BY created_at desc;';
-    }else{
-        $sql  = 'SELECT * FROM contacts';
-        $sql .= '   ORDER BY created_at desc;';
+    $sql = 'UPDATE contacts';
+    if($_POST['note']){
+        $id=$_POST['id'];
+        $note=$_POST['note'];
+        $sql .= '   SET note="'.$note.'"';
+        $sql .= '   WHERE id="'.$id.'"';
+     }
+    $conn->execute($sql);
+
+    $sql  = 'SELECT * FROM contacts';
+    if($_POST['search']){
+        $search=$_POST['search'];
+        $sql .= '   WHERE name LIKE "%'.$search.'%"';
     }
+    $sql .= '   ORDER BY created_at desc;';
 
     $contacts = $conn->fetch($sql);
-    var_dump($contacts);
+
+    //var_dump($contacts);
+    var_dump($id);
+    var_dump($_POST['note']);
     var_dump($sql);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <title>2018/04/06の課題</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -51,7 +53,7 @@
                 <form method="post">
                     <div class="form-group">
                         <label for="InputSeachName">名前検索</label>
-                        <input type="text" class="form-control" name="seach">
+                        <input type="text" class="form-control" name="search">
                     </div>
                     <input type="submit" class="btn btn-success" value="検索">
                 </form>
@@ -67,7 +69,7 @@
                         <th>メール</th>
                         <th>内容</th>
                         <th>登録日</th>
-                        <th>メモ</th>
+                        <th>note</th>
                     </tr>
 
                     <?php
@@ -78,18 +80,21 @@
                         echo '<td>'.$val['mail'].'</td>';
                         echo '<td>'.$val['content'].'</td>';
                         echo '<td>'.$val['created_at'].'</td>';
-                        echo '</tr>';
+                    ?>
+
+                    <td>
+                        <form method="post">
+                            <textarea name="note"><?php echo $val['note']; ?></textarea>
+                            <input type="submit" class="btn btn-success" value="送信">
+                            <input type="hidden" name="id" value="<?php echo $val['id']; ?>">
+                        </form>
+                    </td>
+
+                    <?php echo '</tr>';
                     } ?>
                 </table>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-xs-12 text-center">
-                <input type="button" onclick="location.href='index.php'"value="TOPに戻る"　class="backbtn">
-            </div>
-        </div>
-    </div>
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>

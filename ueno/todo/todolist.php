fcc882ca0b45af1daddf740eb1e95b9387dcc846
ueno <ueno@genius-web.co.jp>
2018-04-23 14:47:30 +0900

    #75　インデント処理済

--- ueno/todo/todolist.php
+++ ueno/todo/todolist.php
@@ -4,13 +4,13 @@
 <title>Todoリスト</title>
 <head>
     <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="./assets/css/app.css">
-  <script type="text/javascript" src="./assets/js/app.js"></script>
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <script type="text/javascript" src="./assets/js/app.js"></script>
 </head>
 <body>
     <div class="container">
@@ -23,7 +23,7 @@
     <input type="text" name="content" class="form-control" required>
 
 
-
+    
 
     <!-- ※完了ページ -->
     <input type="text" name="content" class="form-control" disabled>

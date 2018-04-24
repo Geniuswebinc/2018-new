316034a57e10a4aeb2c6c8241ec21e35d25c15a3
nakatake <nakatake@genius-web.co.jp>
2018-04-24 17:57:05 +0900

    #84 各ステータスで一覧表示するように

--- nakatake/todo/index.php
+++ nakatake/todo/index.php
@@ -1,3 +1,58 @@
+<?php
+require_once dirname(__FILE__) .'/data/require.php';
+// データベース接続のクラス
+$conn = new DbConn();
+
+$name = $_POST['taskdata'];
+$priority = $_POST['priority'];
+
+if ($name) {
+    $sql = 'INSERT INTO';
+    $sql .= ' todo_lists(task_name, priority)';
+    $sql .= ' VALUES ( ';
+    $sql .= ' "'.$name.'", "'.$priority.'"';
+    $sql .= ' )';
+    $conn->fetch($sql);
+}
+
+if ($_POST['nocomplete_id']) {
+    $nocomplete_id = $_POST['nocomplete_id'];
+    $sql = 'UPDATE todo_lists';
+    $sql .= ' SET states = 2, updated_at = CURRENT_TIMESTAMP';
+    $sql .= ' WHERE id = '.$nocomplete_id;
+    $conn->execute($sql);
+}
+
+if ($_POST['complete_id']) {
+    $complete_id = $_POST['complete_id'];
+    $sql = 'UPDATE todo_lists';
+    $sql .= ' SET delete_flag = 1';
+    $sql .= ' WHERE id = '.$complete_id;
+    $conn->execute($sql);
+}
+
+// 優先度テーブル取得
+$sql = 'SELECT * FROM priorities';
+$sql .= ' ORDER BY numbers';
+$priorities = $conn->fetch($sql);
+
+
+$sql  = 'SELECT t.*, p.name FROM todo_lists t';
+$sql .= ' LEFT OUTER JOIN priorities p';
+$sql .= ' ON t.priority = p.id';
+// 未完了用
+$sql2 = $sql.' WHERE t.states = 1';
+$sql2 .= ' ORDER BY t.created_at DESC';
+// 完了
+$sql3 = $sql. ' WHERE t.states = 2';
+$sql3 .= ' AND delete_flag = 0';
+$sql3 .= ' ORDER BY t.updated_at DESC';
+
+$tasks_nocomplate = $conn->fetch($sql2);
+$tasks_complete = $conn->fetch($sql3);
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -20,48 +75,74 @@
                 </div>
                 <div class="col-xs-4">
                     <select class="form-control" name="priority">
-                        <option value="1">高</option>
-                        <option value="2" selected>中</option>
-                        <option value="3">低</option>
+                        <?php
+                        foreach ($priorities as $priority) {
+                            ?>
+                            <option value="<?php echo $priority['id']; ?>" <?php if ($priority['name'] == "中") echo "selected"; ?>>
+                                <?php echo $priority['name']; ?>
+                            </option>
+                            <?php
+                        }
+                        ?>
                     </select>
                 </div>
                 <div class="col-xs-4">
-                    <input type="submit" value="登録">
+                    <input type="submit" value="登録" id="btnSubmit">
                 </div>
             </form>
         </div>
 
         <div class="row">
-            <form method="post">
+            <div class="col-xs-12">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#A" data-toggle="tab" id="tabA">未完了</a></li>
+                    <li><a href="#B" data-toggle="tab" id="tabB">未完了</a></li>
+                </ul>
 
-                <div class="col-xs-12">
-                    <ul class="nav nav-tabs">
-                        <li class="active"><a href="#A" data-toggle="tab">未完了</a></li>
-                        <li><a href="#B" data-toggle="tab">未完了</a></li>
-                    </ul>
-
-                    <div class="tab-content">
-                        <div class="tab-pane active" id="A">
-                            <table class="table text-center">
+                <div class="tab-content">
+                    <div class="tab-pane active" id="A">
+                        <table class="table text-center">
+                            <?php
+                            foreach ($tasks_nocomplate as $task) {
+                                ?>
                                 <tr>
-                                    <td>中</td>
-                                    <td>内容</td>
-                                    <td></td>
+                                    <?php echo '<td>'.$task['id'].'</td>'; ?>
+                                    <?php echo '<td>'.$task['name'].'</td>'; ?>
+                                    <?php echo '<td>'.$task['task_name'].'</td>'; ?>
+                                    <td>
+                                        <form method="post">
+                                            <input type="submit" value="完了">
+                                            <input type="hidden" name="nocomplete_id" value="<?php echo $task['id']; ?>">
+                                        </form>
+                                    </td>
                                 </tr>
-                            </table>
-                        </div>
-                        <div class="tab-pane" id="B">
-                            <table class="table text-center">
+                                <?php
+                            }
+                            ?>
+                        </table>
+                    </div>
+                    <div class="tab-pane" id="B">
+                        <table class="table text-center">
+                            <?php
+                            foreach ($tasks_complete as $task2) {
+                                ?>
                                 <tr>
-                                    <td>高</td>
-                                    <td>内容2</td>
-                                    <td></td>
+                                    <?php echo '<td>'.$task2['id'].'</td>'; ?>
+                                    <?php echo '<td>'.$task2['task_name'].'</td>'; ?>
+                                    <td>
+                                        <form method="post">
+                                            <input type="submit" value="削除">
+                                            <input type="hidden" name="complete_id" value="<?php echo $task2['id']; ?>">
+                                        </form>
+                                    </td>
                                 </tr>
-                            </table>
-                        </div>
+                                <?php
+                            }
+                            ?>
+                        </table>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
